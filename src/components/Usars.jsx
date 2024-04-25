2ec7de59efe22ba9013fa87fb0983ec32b36464d
Md. Salauddin <salauddinahmed555@gmail.com>
2024-04-25 12:39:07 +0600

    first commit

---
+++ src/components/Usars.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+const Usars = () => {
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+
+  const handleDelete = (id) => {
+    // make sure user is confirmed to delete
+    fetch(
+      `https://coffee-store-server-cl.vercel.app/user/${id}`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          console.log("deleted successfully");
+
+          // remove the user from UI with filter
+          const remainingUsers = users.filter((user) => user._id !== id);
+          setUsers(remainingUsers);
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h1>Users: {loadedUsers.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Email</th>
+              <th>Created At</th>
+              <th>Last Logged In</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.length && users?.map((user) => (
+              <tr key={user._id}>
+                <td>1</td>
+                <td>{user.email}</td>
+                <td>{user.createdAt}</td>
+                <td>{user.lastLoggedAt}</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn"
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Usars;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC72AQIYSrEpdWNHJTQQ9YttDpe8O9r3rE",
+  authDomain: "coffee-store-26ed7.firebaseapp.com",
+  projectId: "coffee-store-26ed7",
+  storageBucket: "coffee-store-26ed7.appspot.com",
+  messagingSenderId: "700897058722",
+  appId: "1:700897058722:web:23424a100dd6cc11301cfb"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
